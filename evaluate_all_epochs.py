e1050ac2ea3b55634a4887295de9f73a22c975da
odgiiv <odgiiv.kh@gmail.com>
2019-08-09 17:35:28 +0200

    run evaluate for all epoch weights in dir at once

---
+++ evaluate_all_epochs.py
@@ -0,0 +1,29 @@
+import argparse, os
+from evaluate import eval
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--model_name", help="Name of directory of specific model in ./models parent directory, such as unet, attention-unet or segan.")
+parser.add_argument("-d", "--dataset_path", required=True)
+parser.add_argument("-wp", "--weights_path", help="Full path to the weight file.")
+parser.add_argument("-i", "--exp_id", type=int, required=True)
+
+args = parser.parse_args()
+
+assert(args.model_name in ['unet', 'attentionUnet'])
+
+sys.path.append('./models/unet')
+
+if args.model_name == 'unet':
+    model_dir = './models/unet'    
+    from base_model import Unet
+    model = Unet()
+
+elif args.model_name == 'attentionUnet' :        
+    model_dir = './models/attentionUnet'
+    sys.path.append(model_dir)
+    from model import AttentionalUnet
+    model = AttentionalUnet()
+
+for f in os.listdir(args.weights_path):
+    print("loading weight: {}".format(f))
+    eval(model, os.path.join(args.weights_path, f), True, args.dataset_path, args.exp_id)
