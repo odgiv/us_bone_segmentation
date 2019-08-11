3f748ca5cddbd038c879baf71dba90809d202dde
odgiiv <odgiiv.kh@gmail.com>
2019-08-11 04:05:12 +0200

    show train seg result

--- train.py
+++ train.py
@@ -171,8 +171,8 @@ for imgs, labels in train_gen:
         if global_step.numpy() % model_params["save_summary_steps"] == 0:
             logging.info("step {}, seg_loss {:.4f}, batch_hd {:.4f}, batch_IoU {:.4f}, batch_dice {:.4f}, batch_combi {:.4f}".format(current_step, seg_loss, batch_hd, batch_IoU, batch_dice, batch_combi))
     
-        # seg_results = segmentor_net(tf.image.convert_image_dtype(imgs, tf.float32))
-        # seg_results = tf.argmax(seg_results, axis=-1, output_type=tf.int32)
-        # seg_results = tf.expand_dims(seg_results, -1)
+        seg_results = segmentor_net(tf.image.convert_image_dtype(imgs, tf.float32))
+        seg_results = tf.argmax(seg_results, axis=-1, output_type=tf.int32)
+        seg_results = tf.expand_dims(seg_results, -1)
 
-        # tf.contrib.summary.image("seg_result", tf.cast(seg_results * 255, tf.uint8))
+        tf.contrib.summary.image("seg_result", tf.cast(seg_results * 255, tf.uint8))
