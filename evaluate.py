874ebb756e1f19b1c1f55561add309b63761f7ef
odgiiv <odgiiv.kh@gmail.com>
2019-08-16 15:37:48 +0200

    thinning

--- evaluate.py
+++ evaluate.py
@@ -71,7 +71,6 @@ def eval(model, model_dir, weight_file_path, store_imgs, dataset_path, ex_id, th
             labels, num = find_connected(pred_np, return_num=True)
             for n in range(1, num):
                 if np.sum(labels == n) <= 20:
-
                     new_pred[labels == n] = 0
             
             pred_np = new_pred
